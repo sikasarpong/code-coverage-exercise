77c78f9eab4913e586c86f9e6ca0f9086288ced6
lisafeng <lisafenglisa@yahoo.com>
2022-09-30 13:56:40 -0400

    fixed to get 100% coverage

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,17 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_init_if_no_courses_return_empty():
+    name = "Ada Lovelace"
+    level = "sophomore"
+    courses = None
+
+    ada = Student(name, level, courses)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == []
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -42,3 +53,5 @@ def test_get_student_with_more_classes():
     )
 
     # TODO: write assertions
+    assert get_student_with_more_classes(charles, ada) == ada
+    assert get_student_with_more_classes(ada, charles) == ada
